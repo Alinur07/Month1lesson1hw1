c76f78504665561644cc3dbde16b47ee0d38337f
Alinur07 <arthuralimzhanov@gmail.com>
2022-05-12 16:42:32 +0300

    зделал дз

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите Ваше имя");
+        int word = in.nextInt();
+        System.out.printf("Введите Ваше имя: %d \n",word);
+        in.close();
+
+
+ }
+
+       }
